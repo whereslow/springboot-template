8901e3173e35950512c30a5702e5bef0667fb347
whereslow <liruiyang2016@outlook.com>
2024-10-11 09:46:58 +0800

    项目结构

---
+++ src/test/java/com/sd_studio/SpringbootTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sd_studio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
